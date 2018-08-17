71c8595aae1b96d7a0c987901fe4b804a80c986b
liweili50 <602356374@qq.com>
2018-08-17 20:15:15 +0800

    更多页
    新增更多页面

--- pages/more/index.js
+++ pages/more/index.js
@@ -5,16 +5,38 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    userInfo: null
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    let that = this;
+    // 查看是否授权
+    wx.getSetting({
+      success: function (res) {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+          wx.getUserInfo({
+            success: function (res) {
+              console.log(res.userInfo)
+              that.setData({
+                userInfo: res.userInfo
+              })
+            }
+          })
+        }
+      }
+    })
   },
-
+  bindGetUserInfo: function (e) {
+    console.log(e.detail.userInfo)
+    this.setData({
+      userInfo: e.detail.userInfo
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
